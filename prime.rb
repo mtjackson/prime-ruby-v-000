7d9e95be402657ac4b72814f442bcd9b355fa607
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-27 14:24:12 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,12 @@
-def prime?(integer)
+def prime?(test_number)
   i = 2
-  while i < i
+  while i < test_number
+    if test_number % i == 0
+      return false
+    else
+      return true
+    end
+  end
   numbers = [1..i]
   numbers.any? do |test_number|
     (i % test_number).is_an_int?
