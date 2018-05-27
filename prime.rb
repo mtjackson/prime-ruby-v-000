126e5d2aeecd5d1fd86ee0798429e6af624be36e
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-27 14:27:58 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,14 @@
 def prime?(test_number)
   if test_number < 0
-  i = 2
-  while i < test_number
-    if test_number % i == 0
-      return false
+    return false
+  else 
+    i = 2
+    while i < test_number
+      if test_number % i == 0
+        return false
+      end
+      i +=1
     end
-    i +=1
+    return true
   end
-  return true
 end
